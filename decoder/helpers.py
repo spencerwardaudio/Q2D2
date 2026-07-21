4d4c9b77e21fce3effa5d3955b2d79bb29c0e28a
Spencer Ward <spencerwardaudio@gmail.com>
2026-07-21 20:20:10 +0100

    Fixed drawing array problem

--- decoder/helpers.py
+++ decoder/helpers.py
@@ -17,8 +17,9 @@ def save_figure_to_numpy(fig: plt.Figure) -> np.ndarray:
     Returns:
         ndarray: Numpy array representing the figure.
     """
-    data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep="")
-    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    fig.canvas.draw()
+    buf = np.frombuffer(fig.canvas.buffer_rgba(), dtype=np.uint8)
+    data = buf.reshape(fig.canvas.get_width_height()[::-1] + (4,))[:, :, :3]
     return data
 
 
